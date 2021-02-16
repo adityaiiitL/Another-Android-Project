6bd16e9cc40ac9c2ed669f974d94760f6b43e9b8
Aditya <lit2020023@iiitl.ac.in>
2021-02-16 15:30:56 +0530

    Git

---
+++ app/src/main/java/com/example/dilmilao/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.dilmilao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    EditText male,female;
+    TextView tv;
+    String result;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView im= (ImageView) findViewById(R.id.imageView);
+        im.setImageResource(R.drawable.heart_1);
+    }
+    public void showResult(View v){
+        male=(EditText) findViewById(R.id.editTextTextPersonName);
+        female=(EditText) findViewById(R.id.editTextTextPersonName2);
+        tv=(TextView) findViewById(R.id.textView2);
+        String f=female.getText().toString();
+        String m=male.getText().toString();
+        String sum=m+f;
+        sum=sum.toLowerCase();
+        int value=sum.hashCode();
+
+        Random random=new Random(value);
+        result=(random.nextInt(100)+1)+"%";
+        tv.setText(result);
+
+    }
+}
